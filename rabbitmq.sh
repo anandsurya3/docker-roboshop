ac7df270ee856f030ec8bbd08da97d16f97a061f
Anand <kallaanand0@gmail.com>
2026-04-24 15:04:00 +0530

    docker

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -1,2 +1,3 @@
 docker run -d --name rabbitmq --network roboshop -e RABBITMQ_DEFAULT_USER=roboshop -e RABBITMQ_DEFAULT_PASS=roboshop123 rabbitmq:3
 # for install rabbitmq databsae
+docker run -d --name redis --network roboshop redis:7
